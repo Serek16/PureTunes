1631173ba36ea57e50f406cd5c0217563bcf8642
Sergiusz <xbxbcvbxc@gmail.com>
2023-08-24 19:35:44 +0200

    Implement result audio waveform visualisation
    Former-commit-id: f705503a66faa22e31f5197de038e115fac6f66b

--- EmySoundProject/Pages/MainPage.razor.cs
+++ EmySoundProject/Pages/MainPage.razor.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using EmySoundProject.Components;
 using EmySoundProject.Services;
 using EmySoundProject.Exceptions;
 using EmySoundProject.Models;
@@ -10,6 +11,7 @@ using Microsoft.AspNetCore.Components;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using Radzen;
+using SoundFingerprinting.Query;
 
 namespace EmySoundProject.Pages;
 
@@ -21,6 +23,7 @@ public partial class MainPageComponent
 
     [Inject] protected ILogger<MainPageComponent> Logger { get; set; }
 
+    protected RenderFragment WaveformDisplay;
 
     private async Task UpdateEmy()
     {
@@ -90,9 +93,25 @@ public partial class MainPageComponent
 
 
         var time0 = DateTime.Now;
-        var result = await AfmService.FindMatches(FilePath, Confidence / 100d);
 
-        if (!result.Any())
+        List<ResultEntry> matches = await AfmService.FindMatches(FilePath, Confidence / 100d);
+        var waveformTask = CreateResultWaveform(FilePath);
+        await ExtractAudioClips(matches);
+
+        var timeSpan = DateTime.Now.Subtract(time0);
+        Logger.LogInformation("The process has been successfully completed in {TimeSpan} seconds.",
+            timeSpan.ToString("ss'.'ff"));
+        NotificationService.Notify(new NotificationMessage
+            { Duration = 1000, Severity = NotificationSeverity.Success, Summary = "Wygenerowano rezultat!" });
+
+        await waveformTask;
+
+        return JsonConvert.SerializeObject(matches, Formatting.Indented);
+    }
+
+    private async Task ExtractAudioClips(List<ResultEntry> matches)
+    {
+        if (!matches.Any())
         {
             Logger.LogInformation("There was no matching tracks. The process stops.");
             NotificationService.Notify(new NotificationMessage
@@ -100,17 +119,24 @@ public partial class MainPageComponent
                 Duration = 1000, Severity = NotificationSeverity.Success,
                 Summary = "Nie znaleziono żadnych pasujących reklam w audycji!"
             });
-            return JsonConvert.SerializeObject(string.Empty, Formatting.Indented);
+            return;
         }
 
-        await AudioExtractionService.FileGenerate(result, FilePath);
-        var timeSpan = DateTime.Now.Subtract(time0);
+        await AudioExtractionService.FileGenerate(matches, FilePath);
+    }
 
-        Logger.LogInformation("The process has been successfully completed in {TimeSpan} seconds.",
-            timeSpan.ToString("ss'.'ff"));
-        NotificationService.Notify(new NotificationMessage
-            { Duration = 1000, Severity = NotificationSeverity.Success, Summary = "Wygenerowano rezultat!" });
-        return JsonConvert.SerializeObject(result, Formatting.Indented);
+    private async Task CreateResultWaveform(string filePath)
+    {
+        var peaks = AudioPeaksReader.ReadAudioPeaks(filePath, 0.01);
+        WaveformDisplay = builder =>
+        {
+            builder.OpenComponent<WaveformDisplay>(0);
+            builder.AddAttribute(0, "AudioFilePath", filePath);
+            builder.AddAttribute(0, "WaveformRegionModels", AudioExtractionService.WaveformRegionModels);
+            builder.AddAttribute(0, "AudioPeaks", peaks);
+            builder.CloseComponent();
+        };
+        StateHasChanged(); // Request UI to be re-rendered. @waveformDisplay is now visible
     }
 
     private List<PathModel> GetDirectories()
